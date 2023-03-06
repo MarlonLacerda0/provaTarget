3a68c1383debc24f6d15abeaf944da0663c236f6
MarlonLacerda0 <marlonlac10@hotmail.com>
2023-03-06 01:03:25 -0300

    Questão 1 da Prova da Target

---
+++ ex01.py
@@ -0,0 +1,21 @@
+#####################################
+# Autor: Marlon Lacerda de Oliveira
+# Data Criação: 05/03/2023
+# EX 01: Prova TARGET
+#####################################
+def main():
+
+    # Entrada de dados e Declaração de variáveis
+    indice = 13
+    soma = 0
+    k = 0
+    # Processamento
+    while (k < indice):
+        k+=1
+        soma+=k
+
+    print(soma)
+
+
+if __name__ == "__main__":
+    main()
