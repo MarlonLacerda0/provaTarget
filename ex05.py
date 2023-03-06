4041a7455069fdd9f39bd1a3cdeabf0bd76d9fcc
MarlonLacerda0 <marlonlac10@hotmail.com>
2023-03-06 01:10:28 -0300

    Questão 5 da Prova da Target

---
+++ ex05.py
@@ -0,0 +1,17 @@
+#####################################
+# Autor: Marlon Lacerda de Oliveira
+# Data Criação: 05/03/2023
+# EX 05: Prova TARGET
+#####################################
+def main():
+
+    # Entrada de dados
+    s = ("MARLON")
+    # Processamento
+    for i in range(len(s) - 1, -1, -1):
+        print(s[i], end="")
+
+
+
+if __name__ == "__main__":
+    main()
